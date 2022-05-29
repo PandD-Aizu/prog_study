6ad127287f49936ba46f8b3f59b2694fdaabadf6
zawakasutera <zawazawanomorichan@outlook.jp>
2022-05-29 18:11:53 +0900

    ex4-1.c

---
+++ ex04/ex4-1.c
@@ -0,0 +1,51 @@
+/*
+ * テストの点数を格納する構造体を宣言して平均と分散を求めよ
+ */
+
+#include <stdio.h>
+
+struct Student {
+    int english;
+    int math;
+    int japanese;
+};
+
+// a - bの二乗を返す関数
+double getPow(double a, double b) {
+    return (a - b) * (a - b);
+}
+
+// int型同士の割り算でdoubleを返すには、doubleへのキャストが必要です！
+// (第一回勉強会資料を参照してください)
+double getAverage(struct Student s) {
+    return (double)(s.english + s.math + s.japanese) / (double)3;
+}
+
+// 分散は偏差の二乗の平均をとります
+// 偏差は値ー平均です。（どうせ二乗するので平均ー値でも大丈夫です）
+double getDisp(struct Student s) {
+    double ave = getAverage(s);
+    return (getPow(ave, s.english) + getPow(ave, s.math) + getPow(ave, s.japanese)) / (double)3;
+}
+
+int main() {
+    struct Student s;
+    printf("英語の点数を入力してください> ");
+    scanf("%d", &s.english);
+
+    printf("数学の点数を入力してください> ");
+    scanf("%d", &s.math);
+
+    printf("国語の点数を入力してください> ");
+    scanf("%d", &s.japanese);
+
+    printf("平均: %lf, 分散: %lf\n", getAverage(s), getDisp(s));
+}
+
+/* 
+ * 工夫ポイントは
+ * getDisp内でgetAverageを呼びだし、二回平均を求めるプログラムを書く所を一回に抑えている
+ * getPowを定義して、getDispのれreturn文を短く書いている
+ *
+ * 繰り返し書く必要がでるような部分は関数に切り分けることを心がけましょう
+ */
