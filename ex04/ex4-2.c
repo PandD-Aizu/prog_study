4404981635e9bcd678e7eccc2caed1eaacbace28
Roku-3 <down01134@gmail.com>
2022-06-30 19:04:09 +0900

    経路path構造体配列　解説

---
+++ ex04/ex4-2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+
+// 構造体Pointを作成
+typedef struct
+{
+    double x;
+    double y;
+}Point;
+
+int main() {
+    // Pointの配列path, 経路の合計値を格納するsumを定義
+    Point path[4];
+    double sum = 0;
+
+    // Pointを入力
+    for(int i=0; i<4; i++) {
+        scanf("%lf%lf", &path[i].x, &path[i].y);
+    }
+
+    // pathに含まれる点を二つずつ選択し、その距離をsumに足していく
+    for(int i=0; i<3; i++) {
+        double length;
+        Point from = path[i];
+        Point to = path[i+1];
+        length = sqrt((from.x-to.x)*(from.x-to.x)+(from.y-to.y)*(from.y-to.y));
+        sum += length;
+    }
+
+    printf("経路は%f", sum);
+}
